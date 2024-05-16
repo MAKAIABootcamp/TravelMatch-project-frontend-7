0518edf17a0b3adabd32eebfc863289d115790c1
erikagalvisdev <143899498+erikagalvisdev@users.noreply.github.com>
2024-05-16 18:35:28 -0500

    Se realizan cambios en componente SquareTest y se reorganiza estructura de preguntas

--- TravelMatch/src/components/ProfileTest/SquareTest.jsx
+++ TravelMatch/src/components/ProfileTest/SquareTest.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import ButtonTest from './ButtonTest';
+import './SquareTest.scss';
 
-const SquareTest = ({title, option}) => {
+const SquareTest = ({ title, option }) => {
+  const respuestas = Object.entries(option);
   return (
-    <div><h2>{title}</h2>
-    <h4>{option}</h4></div>
-    
-  )
-}
+    <div className = 'askBox'>
+      <h2>{title}</h2>
+      <section>
+        {respuestas.map((item, index) => (
+          <label key={index}>
+            <input type="checkbox" />
+            {`${item[0]}: ${item[1]}`}
+          </label>
+        ))}
+      </section>
+      <ButtonTest test="Siguiente" />
+    </div>
+  );
+};
 
 export default SquareTest;
--- TravelMatch/src/pages/Test/Test.jsx
+++ TravelMatch/src/pages/Test/Test.jsx
@@ -1,49 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./test.scss";
 import ButtonTest from "../../components/ProfileTest/ButtonTest";
 import fondo from "../../assets/fondoTest.jpeg";
 import SquareTest from "../../components/ProfileTest/SquareTest";
+import "./test.scss";
 
 
 function Test() {
  
-  const cardItems = [
-    { id: 1, nombre: "¿Qué clima prefieres?", precio: 10 },
-    // { id: 2, nombre: "Clima tropical húmedo", precio: 10 },
-    // { id: 3, nombre: "Clima de montaña", precio: 10 },
-    { id: 4, nombre: "Clima de páramo", precio: 10, 
-    description: [
-      { id: 1, nombre: "Clima tropical húmedo", precio: 10 },
-      { id: 2, nombre: "Producto 2", precio: 20 },
-      { id: 3, nombre: "Producto 3", precio: 30 },
-      { id: 4, nombre: "Producto 4", precio: 50 },
-    ] },
-    
-  ];
+  const preguntas =[
+    {
+      id: 1,
+      title: '¿Qué clima prefieres?',
+      respuestas: {
+        a: 'Clima tropical húmedo',
+        b: 'Clima de montaña',
+        c: 'Clima de páramo',
+        d: 'Clima tropical de sabana'
+      }
+    },
+    {
+      id: 2,
+      title: '¿Cuál es tu principal motivo para viajar?',
+      respuestas:{
+        a: 'Vacaciones',
+        b: 'Aventura',
+        c: 'Cultura',
+        d: 'Deporte'
+      }
+    },
+    {
+      id: 3,
+      title: 'Prefieres viajar',
+      respuestas:{
+        a: 'Solo',
+        b: 'En pareja',
+        c: 'Con familia',
+        d: 'Con amigos'
+      }
+    }
+  ]
     
   return (
     <div
       className="profile-test-container"
-      style={{
-        backgroundImage: `url(${fondo})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "77vh",
-      }}
     >
-      <div className="background-sombra">
-        {cardItems.map((item) => (
+      
+        {preguntas.map((item) => (
           <SquareTest
             key={item.id}
-            title={item.nombre}
-            option={`$${item.precio}`}
-                   
+            title={item.title}
+            option={item.respuestas}                   
           />
         ))}
 
-        <ButtonTest test="Siguiente" />
-      </div>
+        
     </div>
   );
 }
