9d544061016f8cb5dd6f7d6290049aab3fab5fc4
DavisonC24 <156716312+DavisonC24@users.noreply.github.com>
2024-05-17 15:13:44 -0500

    Se agrega Pagina contactos
    Se agrega El Header Dinamico

--- TravelMatch/src/components/header/header.jsx
+++ TravelMatch/src/components/header/header.jsx
@@ -1,28 +1,38 @@
 import { Link } from "react-router-dom";
 import Carro from "../../assets/iconoCarro.png";
-import "../header/header.scss"
+import "../header/header.scss";
 
 export default function header() {
-    return (
-        <>
-            <div className="header">
-                <Link to={"/"} className="no-underline">
-                    <div className="logo">
-                        <h1>Travel</h1>
-                        <img src={Carro}></img>
-                        <h2>Match</h2>
-                    </div>
-                </Link>
-                <nav className="horizontal-menu">
-                    <ul className="menuA">
-                        <Link><li>Blog</li></Link>
-                        <li>Destinos</li>
-                        <li>Contacto</li>
-                        <Link to={"/register"}><li>Registro</li></Link>
-                        <Link to={"/login"}><li>Entrar</li></Link>
-                    </ul>
-                </nav>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="header">
+        <Link to={"/"} className="no-underline">
+          <div className="logo">
+            <h1>Travel</h1>
+            <img src={Carro}></img>
+            <h2>Match</h2>
+          </div>
+        </Link>
+        <nav className="horizontal-menu">
+          <ul className="menuA">
+            <Link>
+              <li>Blog</li>
+            </Link>
+            <Link to={"/destinos"}>
+              <li>Destinos</li>
+            </Link>
+            <Link to={"/Contacto"}>
+              <li>Contacto</li>
+            </Link>
+            <Link to={"/register"}>
+              <li>Registro</li>
+            </Link>
+            <Link to={"/login"}>
+              <li>Entrar</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 }
