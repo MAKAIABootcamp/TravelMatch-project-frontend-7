d3fedd47b3f64106b22d113b821d717bf3bb5360
DavisonC24 <estefania.martinez@est.iudigital.edu.co>
2024-05-17 15:14:47 -0500

    Revert "Se agrega Pagina contactos"
    This reverts commit 9d544061016f8cb5dd6f7d6290049aab3fab5fc4.

--- TravelMatch/src/pages/home/Home.jsx
+++ TravelMatch/src/pages/home/Home.jsx
@@ -1,47 +1,44 @@
 import { useEffect, useState } from "react";
-import React from "react";
+import React from 'react';
 import "../home/home.scss";
-import Arvi from "../../assets/Arvi.jpg";
-import Guatape from "../../assets/Guatape.jpg";
-import Jardin from "../../assets/Jardin.jpg";
-import Header from "../../components/header/header";
-import Footer from "../../components/Footer/Footer";
+import Arvi from "../../assets/Arvi.jpg"
+import Guatape from "../../assets/Guatape.jpg"
+import Jardin from "../../assets/Jardin.jpg"
+import Header from "../../components/header/header"
+import Footer from "../../components/Footer/Footer"
 
 const Home = () => {
-  const img = [Arvi, Guatape, Jardin];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const img = [Arvi, Guatape, Jardin];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((current) => (current + 1) % img.length); // Cambia 'images' por 'img'
-    }, 10000);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(current => (current + 1) % img.length); // Cambia 'images' por 'img'
+        }, 10000);
 
-  return (
-    <>
-      <div
-        className="background"
-        style={{ backgroundImage: `url(${img[currentIndex]})` }}
-      >
-        <Header />
-        <div className="contenido_body">
-          <h1 className="body_tittle">Antioquia</h1>
-          <p className="body_text">
-            Viajar por Antioquia es una experiencia personalizada donde cada
-            destino vibra con distintas personalidades. Santa Fe de Antioquia
-            atrae a los amantes de la historia, mientras que el Parque Arví es
-            perfecto para los aventureros. Guatapé inspira a los creativos con
-            sus colores vivos, Jericó ofrece un refugio espiritual y tranquilo,
-            y Medellín cautiva con su vibrante mezcla de modernidad y tradición.
-            Descubre en Antioquia el lugar que resuena con tu alma.
-          </p>
-          <button className="body_button">Descubre tu destino</button>
-        </div>
-      </div>
-    </>
-  );
-};
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            <div className="background" style={{ backgroundImage: `url(${img[currentIndex]})` }}>
+
+            <Header />
+                <div className="contenido_body">
+                    <h1 className="body_tittle">Antioquia</h1>
+                    <p className="body_text">Viajar por Antioquia es una experiencia personalizada donde cada destino vibra con distintas personalidades. Santa Fe de Antioquia atrae a los amantes de la historia, mientras que el Parque Arví es perfecto para los aventureros. Guatapé inspira a los creativos con sus colores vivos, Jericó ofrece un refugio espiritual y tranquilo, y Medellín cautiva con su vibrante mezcla de modernidad y tradición. Descubre en Antioquia el lugar que resuena con tu alma.
+                    </p>
+                    <button className="body_button">Descubre tu destino</button>
+                </div>
+            </div>
+            <Footer />
+
+
+        </>
+
+
+    )
+}
 export default Home;
